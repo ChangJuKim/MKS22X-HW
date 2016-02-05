216ef7e587466708f9638a8561e3057ff750c69d
Chang Ju Kim <kimchangju@yahoo.com>
2016-02-04 22:06:43 -0500

    Complete recursion object

---
+++ 01Recurecur...sion/Recursion.java
@@ -0,0 +1,35 @@
+import java.lang.*;
+
+public class Recursion {
+    public String name() {
+	return "Kim,Chang Ju";
+    }
+
+    public double sqrt(double n) {
+	if (n < 0) {
+	    throw new IllegalArgumentException();
+	}
+	return sqrt(n, n / 30);
+    }
+    
+    public double sqrt(double n, double guess) {
+	if (isClose(n, guess)) {
+	    return guess;
+	}
+	return sqrt(n, ((n / guess) + guess) / 2);
+    }
+
+    public boolean isClose(double n, double guess) {
+	return (Math.abs((guess * guess) - n) / n < .000000001);
+    }
+
+    public static void main(String[]args) {
+	Recursion r = new Recursion();
+	System.out.println(r.sqrt(100));
+	System.out.println(r.sqrt(10));
+	System.out.println(r.sqrt(20));
+	System.out.println(r.sqrt(5));
+	System.out.println(r.sqrt(-3));
+    }
+}
+
