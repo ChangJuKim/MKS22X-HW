98a6d26bdcc4f0596b921fedd30ccfed56127ea5
Chang Ju Kim <kimchangju@yahoo.com>
2016-03-09 03:33:48 -0500

    Finish quick slowly

--- 07QuickSelect/Quick.java
+++ 07QuickSelect/Quick.java
@@ -1,7 +1,7 @@
 import java.util.Random;
 
 public class Quick {
-    private static int[] partition(int[]data, int left, int right) {
+    private static int partition(int[]data, int left, int right) {
 	Random randSeed = new Random();
 	int seed = randSeed.nextInt();
 	
@@ -10,24 +10,57 @@ public class Quick {
 	int index = rand.nextInt(right - left + 1) + left;
 
 	//      	index = 2;
-	System.out.println(data[index]);
+	//	System.out.println(data[index]);
 	data = swap(data, index, right);
 
 	//current index going right to left
 	int rightCounter = 0;
 	for (int i = left; i < right-rightCounter; i++) {
-	    System.out.println(toString(data));
+	    //	    System.out.println(toString(data));
+
 	    //if value should be on the right
 	    if (data[i] > data[right]) {
-		System.out.println("IF STATEMENT: SHOULD BE ON RIGHT");
+		//		System.out.println("IF STATEMENT: SHOULD BE ON RIGHT");
 		swap(data, i, right-rightCounter-1);
 		rightCounter += 1;
 		i-= 1;
 	    }
 	}
-	System.out.println(rightCounter);
+	//	System.out.println(rightCounter);
 	swap(data, right-rightCounter, right);
-	return data;
+	return right-rightCounter;
+    }
+
+    public static int quickselect(int[]data, int k) {
+	//my quickselect actually finds kth smallest. To account for this, (length-k smallest) = k largest
+	return quickselect(data, data.length-k, 0, data.length-1);
+    }
+    
+    private static int quickselect(int[]data, int k, int left, int right) {
+	int partitionIndex = partition(data, left, right);
+	//System.out.println("~~~~~~~PartitionIndex, value: "+partitionIndex+", "+data[partitionIndex]+"~~~~~~~~~~~~~~");
+	//System.out.println("Left, Right: "+left+", "+right);
+	//System.out.println("data: "+toString(data));
+
+
+	//if we randomly got kth largest
+	if (partitionIndex == k) {
+	    // System.out.println("DONE BY LUCK -- ans: "+data[partitionIndex]);
+	    return data[partitionIndex];
+	}
+	//if done partitioning
+	if (left == right) {
+	    //System.out.println("DONE BY LEFT=RIGHT -- ans: "+data[partitionIndex]);
+	    return data[partitionIndex];
+	}
+	//if we need a smaller value
+	if (partitionIndex > k) {
+	    return quickselect(data, k, left, partitionIndex);
+	}
+	if (partitionIndex < k) {
+	    return quickselect(data, k, partitionIndex, right);
+	}
+	return 0;
     }
 
     private static String toString(int[] ary) {
@@ -46,8 +79,17 @@ public class Quick {
 	return data;
     }
 
+    public static String name(){
+	return "6,Kim,ChangJu";
+    }
+
     public static void main(String[]args) {
+	/*
 	int[] ary1 = new int[] {4, 5, 2, 6, 1, 0};
-	System.out.println(toString(partition(ary1, 0, ary1.length-1)));
+	System.out.println(quickselect(ary1, 0));
+	*//*
+	System.out.println(partition(ary1, 3, ary1.length-4));
+	System.out.println(toString(ary1));
+	*/
     }
 }
