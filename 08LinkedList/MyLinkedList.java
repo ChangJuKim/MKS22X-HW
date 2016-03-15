8342c32013f99b1e5688840c56f922adb1dccf6d
Chang Ju Kim <kimchangju@yahoo.com>
2016-03-15 12:39:14 -0400

    Create linkedList class and get,set,add,toString functions

---
+++ 08LinkedList/MyLinkedList.java
@@ -0,0 +1,79 @@
+public class MyLinkedList {
+    public LNode start;
+    public int size;
+
+    public MyLinkedList() {
+	start = new LNode();
+    }
+
+    private class LNode {
+	public int data;
+	public LNode next;
+
+	public LNode() {}
+
+	//cannot handle circle lists
+	//cannot handle empty lists
+	public String toString() {
+	    String ans = "["+data+", ";
+	    LNode current = this;
+
+	    while (current.next != null) {
+		current = next;
+		ans += current.data + ", ";
+	    }
+	    return ans + "]";
+	}
+
+	//adds value to end
+	public boolean add(int value) {
+	    LNode current = this;
+	    while (current.next != null) {
+		current = next;
+	    }
+	    next = new LNode();
+	    next.data = value;
+	    next.next = null;
+	    return true;
+	}
+
+	//gets the value at the index
+	//assumes index is within bounds
+	public int get(int index) {
+	    LNode current = this;
+	    int counter = 0;
+	    while (counter < index) {
+		current = next;
+		counter += 1;
+	    }
+	    return current.data;
+	}
+
+	public boolean set(int index, int value) {
+	    LNode current = this;
+	    int counter = 0;
+	    while (counter < index) {
+		current = next;
+		counter += 1;
+	    }
+	    current.data = value;
+	    return true;
+	}
+    }
+
+    public static void main(String[]args) {
+	MyLinkedList m = new MyLinkedList();
+	m.start.data = 1;
+	System.out.println("One list");
+	System.out.println(m.start.toString()+"\n");
+
+	m.start.add(2);
+	System.out.println("Two list");
+	System.out.println(m.start.toString()+"\n");
+	
+	System.out.println("get0: "+m.start.get(0));
+	System.out.println("get1: "+m.start.get(1));
+	m.start.set(1, 3);
+	System.out.println("get1 after set1: "+m.start.get(1));
+    }
+}
