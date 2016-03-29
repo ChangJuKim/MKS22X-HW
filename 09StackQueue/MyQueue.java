6f9ce7ba6a2e3926ca5cda9213bb0f4edf161011
Chang Ju Kim <kimchangju@yahoo.com>
2016-03-28 20:47:16 -0400

    Create the Q

---
+++ 09StackQueue/MyQueue.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+public class MyQueue<T> extends MyLinkedList<T>{
+    /**
+     * Adds the given item to the rear of the queue.
+     */
+    //adds to tail
+    public void enqueue(T item) {
+	add(0, item);
+    }
+    
+    /**
+     * Removes the front item from the queue and returns it.
+     * @exception java.util.NoSuchElementException if the queue is empty.
+     */
+    //removes from tail
+    public T dequeue() {
+	if (size() == 0) {
+	    throw new NoSuchElementException();
+	} else {
+	    return remove(size()-1);
+	}
+    }
+
+    /**
+     * Returns the front item from the queue without popping it.
+     * @exception java.util.NoSuchElementException if the queue is empty.
+     */
+    public T peek() {
+	if (size == 0) {
+	    throw new NoSuchElementException();
+	} else {
+	    return get(size()-1);
+	}
+    }
+
+    /**
+     * Returns the number of items currently in the queue.
+     */
+    public int size() {
+	return size;
+    }
+    
+    /**
+     * Returns whether the queue is empty or not.
+     */
+    boolean isEmpty() {
+	return size() == 0;
+    }
+}
